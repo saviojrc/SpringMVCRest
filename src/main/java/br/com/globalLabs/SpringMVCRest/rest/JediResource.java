db8ca5c9ff3b678660462f72606c6c376e052a9e
saviojrc <saviojrc.1988@gmail.com>
2021-08-26 09:56:25 -0300

    AJUSTADO O JEDI CONTROLLER
    AJUSTADO O JEDI CONTROLLER

--- src/main/java/br/com/globalLabs/SpringMVCRest/rest/JediResource.java
+++ src/main/java/br/com/globalLabs/SpringMVCRest/rest/JediResource.java
@@ -4,23 +4,27 @@ import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
 import br.com.globalLabs.SpringMVCRest.model.Jedi;
 import br.com.globalLabs.SpringMVCRest.repository.JediRepository;
 
 @RestController
-@RequestMapping("/jedi")
 public class JediResource {
 
 	@Autowired
 	private JediRepository repository;
 
-	@GetMapping
+	@GetMapping("/api/jedi")
 	public List<Jedi> getAllJedi() {
 
 		return repository.findAll();
+	}
+	
+	@GetMapping("api/jedi/{id}")
+	public Jedi getJedi(@PathVariable("id") Long id) {
+		return  repository.findById(id).get();
 	}
 
 }
